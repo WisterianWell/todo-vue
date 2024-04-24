bf1cb6efd44e529872e69578ade552c7fc96f330
Lucas Tanino <lucastanino@live.com>
2024-04-24 20:17:00 -0300

    componentizando a aplicação

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(['tarefas-pendentes']);
+</script>
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes
+        </p>
+    </header>
+</template>
